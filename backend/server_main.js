8404c4be00ab88dbd48ee53174d8af534c76a33a
Ofek Regev Ashkenazi <regevofek@live.com>
2020-11-24 14:58:25 +0200

    updated server host

--- backend/server_main.js
+++ backend/server_main.js
@@ -7,14 +7,13 @@ const UserModel = require('./db_models/user_model');
 const HapiCoockie = require('@hapi/cookie');
 const UserModule = require('./routes/user_auth');
 const PostModel = require('./routes/posts')
-const ls_social_db = Mongoose.connect('mongodb://localhost/ls_social_db', {
+const ls_social_db = Mongoose.connect('mongodb+srv://ofek_regev:Dontry12@cluster0.xrbrp.mongodb.net/ls_social_db?retryWrites=true&w=majority', {
   useNewUrlParser: true
 });
 //Init server
 const init = async () => {
   const server = new Hapi.Server({
-    port: 3000,
-    host: 'localhost'
+    host: 'https://lsocialbackend.onrender.com'
   });
   await server.register(require('hapi-cors'))
   await server.register([
